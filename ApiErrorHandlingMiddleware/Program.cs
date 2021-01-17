a162a44dd87eb6d39dddd61a289806ed567135a2
samuel <samuel.bagattin@gmail.com>
2021-01-17 15:36:26 +0100

    Add missing program.cs

---
+++ ApiErrorHandlingMiddleware/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ApiErrorHandlingMiddleware
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+        }
+    }
+}
